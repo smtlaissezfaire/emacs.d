6c1e1b6b2840464921d350c18fe5ea187e2b2e09
smt <smt@a51eb8d1-28d1-46dc-bbf2-d77fbda2bf46>
2008-06-02 02:56:28 +0000

    * Better compile functions
    git-svn-id: https://thmadb.com/svn/personal_svn/.emacs.d@406 a51eb8d1-28d1-46dc-bbf2-d77fbda2bf46

--- compile-emacs-d.el
+++ compile-emacs-d.el
@@ -1,13 +1,12 @@
 
 ;; byte compile all files in this directory
 
-
-(defvar *special-compile-directories* '("~/.emacs.d/" "/Applications/Emacs.app" "~/src/git/emacs-extensions/"))
+(defvar *my-special-compile-directories* '("~/.emacs.d/" "/Applications/Emacs.app" "~/src/git/emacs-extensions/"))
 
 (defun byte-compile-selected-directories (dir_list)
   (cond 
    ((not (null dir_list))
-    (byte-recompile-directory (car dir_list))
+    (byte-recompile-directory (car dir_list) 0)
     (byte-compile-selected-directories (cdr dir_list)))))
                               
-(byte-compile-selected-directories *special-compile-directories*)
+(byte-compile-selected-directories *my-special-compile-directories*)
