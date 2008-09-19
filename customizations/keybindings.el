989041bd5d7127e14e4dd5077f611138bf157b66
Scott Taylor <scott@railsnewbie.com>
2008-09-19 02:53:29 -0400

    * oops.  forgot keybindings

---
+++ customizations/keybindings.el
@@ -0,0 +1,15 @@
+; easier goto line
+(global-set-key "\C-xg" 'goto-line)
+
+; set comment-region
+(global-set-key "\C-xc" 'comment-region)
+
+; set uncomment-region
+(global-set-key "\C-xu" 'uncomment-region)
+
+; set keybinding for running rspec:
+(global-set-key "\M-s" 'run-specs)
+
+; use C-c C-a to select the whole buffer
+(global-set-key "\C-c\C-a" 'mark-whole-buffer)
+
