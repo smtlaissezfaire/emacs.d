a44c0b3516409a862c315e23bc310a36bbe9830e
Scott Taylor <scott@railsnewbie.com>
2008-09-19 02:44:02 -0400

    * Remove old ruby snippets code

--- ruby-snippets-code.el
+++ ruby-snippets-code.el
@@ -11,37 +11,3 @@
 (defun ruby-snippets-debug-if ()
   (interactive)
   (insert "debugger if $debug == true"))
-
-
-
-;; Switch to test ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;; (defun ruby-snippets-switch-to-test ()
-;;   "Switch from a rails file in app to the corresponding Test::Unit test"
-;;   (interactive)
-;;   (let (filename buffer-file-name)
-;;     (debug)
-;;     (cond ((search filename "controllers")
-;;            (switch-to-test :controller, filename))
-;;           ((search filename "models")
-;;            (switch-to-test :model, filename))
-;;           ((message "Invalid file type!")))))
-
-;; (defun switch-to-test (type, filename)
-;;   (find-file (sub-test-filename type filename)))
-
-;; (defun sub-test-filename (type, filename)
-;;   (let (modified-filename (concat (chop_off_rb filename) "_test.rb"))
-;;     (case type
-;;       (:model
-;;        (gsub "app/models" "test/unit" modified-filename))
-;;       (:controller
-;;        (gsub "app/controllers" "test/functional" modified-filename)))))
-
-;; (defun chop_off_rb (filename)
-;;   (gsub ".rb" "" filename))
-
-;; (ruby-snippets-switch-to-test)
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-
