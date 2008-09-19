3b8940fd47ff066ab6891738f42f879beb456b7d
Scott Taylor <scott@railsnewbie.com>
2008-09-19 03:07:55 -0400

    * More reorganization of init file

--- init.el
+++ init.el
@@ -12,15 +12,36 @@
 (progn (cd "~/.emacs.d")
        (normal-top-level-add-subdirs-to-load-path))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; vendor loading ;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 ; ruby syntax highlighting, ruby-electric:
 (require 'inf-ruby)
 (require 'ruby-electric)
-
 ; rcodetools - use with M-x xmp to capture STDOUT
 (require 'rcodetools)
-
 ; a rails package - lots of useful stuff
 (require 'rails)
+(require 'rinari)
+; flymake, for ruby
+(require 'flymake)
+(require 'erc)
+(autoload 'js2-mode "js2" nil t)
+(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+(setq auto-mode-alist (append '(("\\.js$" . c-mode)) auto-mode-alist))
+(autoload 'git-blame-mode "git-blame"
+  "Minor mode for incremental blame for Git." t)
+(require 'git)
+(require 'gitsum)
+; treetop:
+(load "treetop")
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; colors         ;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ; Rinari - rails support, with different erb colors
 (defface erb-face
@@ -31,17 +52,22 @@
   `((t (:background "grey15")))
   "Default inherited face for ERB tag delimeters"
   :group 'rhtml-faces)
-(require 'rinari)
-
-; flymake, for ruby
-(require 'flymake)
 (set-face-background 'flymake-errline "red4")
 (set-face-background 'flymake-warnline "dark slate blue")
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; random customization ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ; don't insert mule encoding utf-8 string at the top of a file
 (defun ruby-mode-set-encoding())
 
-(require 'erc)
+; use option for the metakey for emacs (aquaemacs / x11)
+(setq mac-option-modifier 'meta)
+
+
+;;; tabs;;;;;;;;;;;;;;;;;;;;
 
 ;; *DONT* use tabs
 (setq-default indent-tabs-mode nil)
@@ -54,41 +80,15 @@
 ;; Use 2 spaces for a "soft tab"
 (setq-default c-basic-offset 2)
 (set-tab-stop-width 2)
-(setq auto-mode-alist (append '(("\\.js$" . c-mode)) auto-mode-alist))
-
-(autoload 'js2-mode "js2" nil t)
-(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
-
-
-; use option for the metakey for emacs (aquaemacs / x11)
-(setq mac-option-modifier 'meta)
-
-; use tramp for remote file editing:
-(load "tramp_customization")
-
-; use git scripts:
-(add-to-list 'load-path "~/.emacs.d/git")
-(autoload 'git-blame-mode "git-blame"
-  "Minor mode for incremental blame for Git." t)
-(require 'git)
-(require 'gitsum)
 
 
 ; my own custom extensions: 
+(load "tramp_customization")
 (load "ruby-snippets-code")
 (load "flymake-ruby")
 (load "spec-runner")
-
-(load "vibrant-ink")
-(set-theme-to-vibrant-ink)
-
 (load "ido-find-in-project")
-
-; erlang support ( needs erlang installed in darwin ports )
 (load "erlang_emacs")
-
-; treetop:
-(load "treetop")
 
 ; remove the toolbar, menubar, scrollbar
 (tool-bar-mode -1)
@@ -127,6 +127,16 @@
 (defalias
   'fp
   (symbol-function 'go-to-flavorpill-directory))
+
+;;;;;;;;;;;;;;;;;;;;
+;  themes
+;
+;;;;;;;;;;;;;;;;;;;
+
+(load "vibrant-ink")
+(set-theme-to-vibrant-ink)
+
+
 
 
 ; load time for emacs.  (also see top of the file)
