956e5c325d9dd5b8597e8490e0876fe74789b17e
Scott Taylor <scott@railsnewbie.com>
2008-09-27 03:34:35 -0400

    * Remove unnecessary .el's in load statements

--- init.el
+++ init.el
@@ -3,19 +3,19 @@
 (set-start-load-time)
 
 ; the load order of the following *is* important
-(load "~/.emacs.d/etc/colors/rinari.el")
-(load "~/.emacs.d/etc/utils.el")
-(load "~/.emacs.d/vendor.el")
+(load "~/.emacs.d/etc/colors/rinari")
+(load "~/.emacs.d/etc/utils")
+(load "~/.emacs.d/vendor")
 (load "~/.emacs.d/customizations/flymake-ruby")
-(load "~/.emacs.d/etc/colors/flymake.el")
+(load "~/.emacs.d/etc/colors/flymake")
 (load "~/.emacs.d/themes/vibrant-ink")
 
 ; the order of the following files is unimportant
 (load "~/.emacs.d/projects/find-in-project/ido-find-in-project")
 (load "~/.emacs.d/customizations/erlang_emacs")
 (load "~/.emacs.d/customizations/emacs_backup_files")
-(load "~/.emacs.d/customizations/tabs.el")
-(load "~/.emacs.d/customizations/full_screen.el")
+(load "~/.emacs.d/customizations/tabs")
+(load "~/.emacs.d/customizations/full_screen")
 (load "~/.emacs.d/customizations/tramp_customization")
 (load "~/.emacs.d/customizations/keybindings")
 (load "~/.emacs.d/etc/snippets/ruby-snippets-code")
@@ -23,5 +23,5 @@
 (load "~/.emacs.d/etc/spec-runner")
 
 ; this should always be loaded last:
-(load "~/.emacs.d/etc/start_up.el")
+(load "~/.emacs.d/etc/start_up")
 (report-total-load-time)
