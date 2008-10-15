0d2fe28d606dc4b52f0b2a5c51679c3745a380a0
Scott Taylor <scott@railsnewbie.com>
2008-10-15 12:07:12 -0400

    * Add snippet aliases.  Add snippet for $debug = true

--- etc/snippets/ruby-snippets-code.el
+++ etc/snippets/ruby-snippets-code.el
@@ -4,10 +4,22 @@
   (insert "require File.dirname(__FILE__) + \"/\"")
   (backward-char))
 
+(defalias 'rrfd 'ruby-snippets-rfd)
+
 (defun ruby-snippets-debug ()
   (interactive)
   (insert "$debug = true; require 'rubygems'; require 'ruby-debug'; debugger"))
 
+(defalias 'rdg 'ruby-snippets-debug)
+
 (defun ruby-snippets-debug-if ()
   (interactive)
   (insert "debugger if $debug == true"))
+
+(defalias 'rdgbc 'ruby-snippets-debug-if)
+
+(defun ruby-snippets-set-debug-flag
+  (interactive)
+  (insert "$debug = true"))
+
+(defalias 'rdebug 'ruby-snippets-set-debug-flag)
