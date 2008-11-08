6c7c61da23f6d4925ac719efdff1f3cbe9320dac
Scott Taylor <scott@railsnewbie.com>
2008-11-08 09:59:24 -0500

    * Refactor out into file for clarity

---
+++ customizations.el
@@ -0,0 +1,14 @@
+(load "~/.emacs.d/customizations/flymake-ruby")
+
+; the order of the following files is unimportant
+(load "~/.emacs.d/projects/find-in-project/ido-find-in-project")
+(load "~/.emacs.d/customizations/erlang_emacs")
+(load "~/.emacs.d/customizations/emacs_backup_files")
+(load "~/.emacs.d/customizations/tabs")
+(load "~/.emacs.d/customizations/full_screen")
+(load "~/.emacs.d/customizations/tramp_customization")
+(load "~/.emacs.d/customizations/keybindings")
+(load "~/.emacs.d/etc/snippets/ruby-snippets-code")
+(load "~/.emacs.d/etc/common_directories")
+(load "~/.emacs.d/etc/spec-runner")
+(load "~/.emacs.d/customizations/emacs_backup_files")
--- init.el
+++ init.el
@@ -6,22 +6,10 @@
 (load "~/.emacs.d/etc/colors/rinari")
 (load "~/.emacs.d/etc/utils")
 (load "~/.emacs.d/vendor")
-(load "~/.emacs.d/customizations/flymake-ruby")
 (load "~/.emacs.d/etc/colors/flymake")
 (load "~/.emacs.d/themes/vibrant-ink")
 
-; the order of the following files is unimportant
-(load "~/.emacs.d/projects/find-in-project/ido-find-in-project")
-(load "~/.emacs.d/customizations/erlang_emacs")
-(load "~/.emacs.d/customizations/emacs_backup_files")
-(load "~/.emacs.d/customizations/tabs")
-(load "~/.emacs.d/customizations/full_screen")
-(load "~/.emacs.d/customizations/tramp_customization")
-(load "~/.emacs.d/customizations/keybindings")
-(load "~/.emacs.d/etc/snippets/ruby-snippets-code")
-(load "~/.emacs.d/etc/common_directories")
-(load "~/.emacs.d/etc/spec-runner")
-(load "~/.emacs.d/customizations/emacs_backup_files")
+(load "~/.emacs.d/customizations")
 
 ; this should always be loaded last:
 (load "~/.emacs.d/etc/start_up")
