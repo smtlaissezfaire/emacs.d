27b7414fb17b83406bffba61358d8f1791df10e9
Scott Taylor <scott@railsnewbie.com>
2008-11-08 11:50:29 -0500

    * fix loading

--- etc/common_directories.el
+++ etc/common_directories.el
@@ -1,14 +1,13 @@
-(defun go-to-dir (dir)
-  (interactive)
-  (find-file dir))
-  
-
 (defconst *gemdir* "/usr/lib/ruby/user-gems/1.8/gems")
 (defun go-to-gemdir nil
-  (go-to-gemdir *gemdir*))
+  (interactive)
+  (find-file *gemdir*))
 (defalias 'gemdir 'go-to-gemdir)
 
 (defconst *flavorpill* "~/src/git/flavorpill_com")
 (defun go-to-flavorpill-directory nil 
-  (go-to-gemdir *flavorpill*))
+  (interactive)
+  (find-file *flavorpill*))
 (defalias 'fp 'go-to-flavorpill-directory)
+
+
