47fa5fdca59c5a2c132418409be6be2d21fa42e5
Scott Taylor <scott@railsnewbie.com>
2008-12-02 18:53:43 -0500

    * Fix hooks: make sure ruby-electric and flymake get turned on with ruby files

--- vendor.el
+++ vendor.el
@@ -3,9 +3,13 @@
 
 (require 'cl)
 (add-to-list 'load-path "~/.emacs.d/vendor/ruby")
-(require 'inf-ruby)
+
 (require 'ruby-mode)
-(require 'ruby-electric)
+(require 'inf-ruby)
+
+;; we need flymake later on in the process for font-faces.
+;; *DONT* put it in ruby-mode-hook
+(require 'flymake)
 
 ; rcodetools - use with M-x xmp to capture STDOUT
 ;; (require 'rcodetools)
@@ -13,7 +17,15 @@
 ; a rails package - lots of useful stuff
 (add-to-list 'load-path "~/.emacs.d/vendor/rinari/")
 (require 'rinari)
-(require 'flymake)
+
+(add-hook 'ruby-mode-hook
+          (lambda nil
+            (require 'ruby-electric)
+            (ruby-electric-mode)
+            (flymake-mode-on)))
+
+
+
 
 ; for chatting on irc
 (require 'erc)
