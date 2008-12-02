279a2f3b31eadedd542fa7bf70d7cf98dc2bfd10
Scott Taylor <scott@railsnewbie.com>
2008-12-01 22:06:29 -0500

    * Add erlang 2.6.2 to load path

--- customizations/erlang_emacs.el
+++ customizations/erlang_emacs.el
@@ -1,6 +1,6 @@
-(setq load-path 
-      (cons  "/opt/local/lib/erlang/lib/tools-2.6.1/emacs"
-             load-path))
+(add-to-list 'load-path "/opt/local/lib/erlang/lib/tools-2.6.1/emacs")
+(add-to-list 'load-path "/opt/local/lib/erlang/lib/tools-2.6.2/emacs")
+
 (setq erlang-root-dir "/opt/local/lib/erlang")
 (setq exec-path (cons "/opt/local/lib/erlang/bin" exec-path))
 (require 'erlang-start)
