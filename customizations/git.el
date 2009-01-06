b9e065df92b37101bff2412e873e0c95b92c4441
Scott Taylor <scott@railsnewbie.com>
2009-01-06 13:18:35 -0500

    * Fix git-blame mode output format (which got messed up when the library was upgraded)

--- customizations/git.el
+++ customizations/git.el
@@ -1,3 +1,10 @@
 
 ;; Set the blame format for git-blame-mode.  Use short commit hash
 (setq git-blame-log-oneline-format "format:[%h, %cr] %cn: %s")
+
+(defun git-describe-commit (hash)
+  (with-temp-buffer
+    (call-process "git" nil t nil
+                  "log" "-1" (concat "--pretty=" git-blame-log-oneline-format)
+                  hash)
+    (buffer-substring (point-min) (1- (point-max)))))
