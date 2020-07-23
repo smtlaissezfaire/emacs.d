a8e96f5e44062f525d4ab2ee863b68cca0f41e33
Scott Taylor <scott@railsnewbie.com>
2020-07-22 23:05:04 -0700

    emacs: it's been a long time...

--- init.el
+++ init.el
@@ -11,9 +11,6 @@
 
 (load "~/.emacs.d/customizations")
 
-; this should always be loaded last:
-(load "~/.emacs.d/etc/start_up")
-(report-total-load-time)
 (custom-set-variables
   ;; custom-set-variables was added by Custom.
   ;; If you edit it by hand, you could mess it up, so be careful.
@@ -21,9 +18,14 @@
   ;; If there is more than one, they won't work right.
  '(ecb-options-version "2.40")
  '(ecb-source-path (quote (("~/src/git/flavorpill_com" "/")))))
+
 (custom-set-faces
   ;; custom-set-faces was added by Custom.
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- )
+)
+
+; this should always be loaded last:
+(load "~/.emacs.d/etc/start_up")
+(report-total-load-time)
